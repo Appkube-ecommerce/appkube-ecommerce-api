cb08aa4357a507a52bc12b7d2d863fff4977017d
Mohammed Ali <mohammed.ali@synectiks.com>
2024-04-01 16:59:47 +0530

    commit

--- webhooks/merge.js
+++ webhooks/merge.js
@@ -22,7 +22,7 @@ async function sendButtons(toNumber, whatsappToken, options) {
 
         console.log("Sending buttons message to number:", toNumber);
 
-        const response = await axios.post("https://graph.facebook.com/v19.0/208582795666783/messages", messageData, requestOptions);
+        const response = await axios.post("https://graph.facebook.com/v19.0//messages", messageData, requestOptions);
         const result = response.data;
 
         console.log("Buttons message sent successfully:", result);
